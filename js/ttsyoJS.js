cfa5a94fc57722f730586dc3db917652e9bf03ac
Sam Peterson <petersonsamj12@gmail.com>
2017-12-19 23:21:09 -0600

    added a js function to handle caption functionality

--- js/ttsyoJS.js
+++ js/ttsyoJS.js
@@ -4,27 +4,38 @@
  */
 
 
-
-//I stole this from stack exchange
-function testFunction() {
-  document.getElementById("demo").innerHTML = "we in business!";
-}
-
-
 /*
 * Function to plaster the nav bar to the top of 
 * the screen when it gets scrolled there
 */
-function stickyNav(){
+function stickyNav() {
 
     var windowTop = $(window).scrollTop();
     var stickyAnchor =  $('#stickyAnchor').scrollTop();
     
-    if(windowTop > stickyAnchor){
+    if (windowTop > stickyAnchor ){
         $('#homeNavBar').addClass('sticky');
     }
-    else{
+     else{
           $('#homeNavBar').removeClass('sticky');
     }
     
+}
+
+
+/*Sets the width of ttsyoCaptions on photos*/
+function setCaptionWidth( img ){
+    //gets the last child of the parent node - div -> <p> and
+    //sets that width the the img width
+    console.log("In the new function");
+    console.log("image width: " + img.width);
+    img.parentNode.lastElementChild.style.width = img.width + "px";
+    img.parentNode.lastElementChild.style.opacity = .9;
+    
+    //set onMouseLeave
+    img.onmouseout = function(){
+        img.parentNode.lastElementChild.style.opacity = 0;
+    }
+
+   
 }
